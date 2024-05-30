e5411e2d0eee9027ee7ef172bb385dcf3a039c60
J-Bills <billsjamal@gmail.com>
2024-05-29 17:26:54 -0700

    Finishing selenium challenge

--- main.py
+++ main.py
@@ -12,13 +12,28 @@ driver.get('https://www.python.org')
 
 # print(f"{price_dollar.text}.{price_cent.text}")
 
-search_bar = driver.find_element(By.NAME, value='q')
-print(search_bar.get_attribute('placeholder'))
-button = driver.find_element(By.ID, value='submit')
-print(button.size)
-doc_link = driver.find_element(By.CSS_SELECTOR, value='.documentation-widget a')
-print(doc_link)
-submit_bug = driver.find_element(By.XPATH, value='//*[@id="site-map"]/div[2]/div/ul/li[3]/a')
-print(submit_bug.text)
-#driver.close()
+# search_bar = driver.find_element(By.NAME, value='q')
+# print(search_bar.get_attribute('placeholder'))
+# button = driver.find_element(By.ID, value='submit')
+# print(button.size)
+# doc_link = driver.find_element(By.CSS_SELECTOR, value='.documentation-widget a')
+# print(doc_link)
+# submit_bug = driver.find_element(By.XPATH, value='//*[@id="site-map"]/div[2]/div/ul/li[3]/a')
+# print(submit_bug.text)
+
+#Challenge:
+
+event_menu = driver.find_element(By.XPATH, value='//*[@id="content"]/div/section/div[2]/div[2]/div/ul')
+events  = event_menu.find_elements(By.TAG_NAME, value='li')
+
+event_dict ={
+    
+}
+
+for index, e in enumerate(events):
+    result = e.text.split('\n')
+    event  = {'time' :result[0], 'name':result[1]}
+    event_dict[str(index)] = event
+
+print(event_dict)
 #driver.quit()
